b814ac5798be29b0631b430a8cdcdd658704e46e
Adam Morris <adam.morris@cern.ch>
2016-02-01 17:14:33 +0100

    comments

--- src/GetTree.cpp
+++ src/GetTree.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 #include <vector>
 using namespace std;
+/*****************************************************************************/
+// Overloading the function with all reasonable ways you might want to call it
 TTree* GetTree(string filename)
 {
   TTree* tree = GetTree(new TFile(filename.c_str()), "");
@@ -27,11 +29,14 @@ TTree* GetTree(string filename, TCut* cut)
   TTree* tree = GetTree(new TFile(filename.c_str()), cut);
   return tree;
 }
+/*****************************************************************************/
 TTree* GetTree(TFile* file, TCut* cut)
 {
   TTree* tree;
+  // This list can be freely extended without having to alter any other lines
   vector<string> treenames = {"DecayTreeTuple/DecayTree"
                              ,"DecayTree"
+                             ,"MCDecayTreeTuple/MCDecayTree"
                              ,"MCDecayTree"};
   for(auto name: treenames) // C++11 magic
   {
