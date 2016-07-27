0eba3f5fab58cf25f82656b4479e9e3d960f7e27
Adam Morris <adam.morris@cern.ch>
2016-07-27 14:00:52 +0200

    don't need these

--- include/maxOfThree.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MAXOFTHREE_H__
-#define __MAXOFTHREE_H__
-#include "TMath.h"
-using namespace std;
-float maxOfThree(double a, double b, double c)
-{
-  return TMath::Max(TMath::Max(a,b),c);
-}
-#endif
