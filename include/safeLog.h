b3f8ffb334fb19bbbfe0e2d476156ccd955567ce
Konstantin Gizdov <kgizdov@gmail.com>
2017-03-09 00:45:57 +0000

    nicer prototype for safe log

--- include/safeLog.h
+++ include/safeLog.h
@@ -1,5 +1,6 @@
 #ifndef __SAFELOG_H__
 #define __SAFELOG_H__
+#include <cmath>
 #include "TMath.h"
 float safeLog(float x)
 {
@@ -7,4 +8,32 @@ float safeLog(float x)
 	if(x>TMath::Exp(-25)) return TMath::Log(x);
 	else return -25;
 }
+
+namespace safe {
+    float log(int x, float low_bound = -INFINITY) {
+        if (x > std::exp(low_bound)) {
+            return std::log(x);
+        }
+        return low_bound;
+    }
+    float log(float x, float low_bound = -INFINITY) {
+        if (x > std::exp(low_bound)) {
+            return std::log(x);
+        }
+        return low_bound;
+    }
+    double log(double x, double low_bound = -INFINITY) {
+        if (x > std::exp(low_bound)) {
+            return std::log(x);
+        }
+        return low_bound;
+    }
+    long double log(long double x, long double low_bound = -INFINITY) {
+        if (x > std::exp(low_bound)) {
+            return std::log(x);
+        }
+        return low_bound;
+    }
+}  // namespace safe
+
 #endif
